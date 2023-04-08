56595fe717c25a59ca0d9b95b9ca4e26a751dab3
dlimaf <dlimaf1995@gmail.com>
2023-04-08 13:33:37 +0000

    terminado

--- src/js/component/jumbotronvistas.js
+++ src/js/component/jumbotronvistas.js
@@ -6,7 +6,7 @@ export const JumbotronVistas = (props) => {
       <div className="container">
       <div className="col-md-12 g-0">
         <div className="position-relative w-100">
-          <div className="position-absolute top-50 start-100 translate-middle mb-5 w-100 p-5 text-end">
+          <div className="position-absolute top-50 start-50 translate-middle mb-5 w-100 p-5 d-flex justify-content-end text-end">
             <h1 className="display-5 col-md-5 fw-normal opacity-80 bg-transparent text-white" style={{fontSize:"50px"}}>{props.titulo1}</h1>
           </div>
           <img
